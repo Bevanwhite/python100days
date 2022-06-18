d3fefb0222b8849ff8491b82ea35e542d76c76c1
Bevanwhite <jefferybevan@gmail.com>
2022-06-18 21:31:09 +0530

    exercise02 - is completed

--- day01.py
+++ day01.py
@@ -3,15 +3,29 @@
 
 
 # Example Output
-
 # After you have written your code, you should run your program and it should print the following:
-
 # Day 1 - Python Print Function
-
 # The function is declared like this:
-
 # print('what to print')
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# using \n 
+# print("Hello world!\nHello world!\nHello world!")
+
+# combining strings
+# print("Hello" + " " + "Beva")
+# print("Hello " + "Beva")
+# print("Hello" + " Beva")
+
+# Day 1 - String Manipulation
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
