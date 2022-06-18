adfe28c9e3cb4c166eccc4d257301f5f06917096
Bevanwhite <jefferybevan@gmail.com>
2022-06-18 22:19:18 +0530

    exercise 03 - is completed

--- day01.py
+++ day01.py
@@ -2,7 +2,7 @@
 # print("Hello world")
 
 
-# Example Output
+# Example 01 Output
 # After you have written your code, you should run your program and it should print the following:
 # Day 1 - Python Print Function
 # The function is declared like this:
@@ -20,12 +20,21 @@
 # print("Hello " + "Beva")
 # print("Hello" + " Beva")
 
+# Example 02 Output
 # Day 1 - String Manipulation
 # String Concatenation is done with the "+" sign.
 # e.g. print("Hello " + "world")
 # New lines can be created with a backslash and n.
 
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# input function
+# input("what is your Name?")
+
+# exercise 03
+# get the number of letter's in the input string
+name = input("what is your name? ")
+print(len(name))
