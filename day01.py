fad45907640fb869a3db8fd022a219696b87a98b
Bevanwhite <jefferybevan@gmail.com>
2022-06-18 20:08:52 +0530

    exercise-01 is done

--- day01.py
+++ day01.py
@@ -1,1 +1,17 @@
-print("hi")
+# print() funtion - inside the quotes only in printing
+# print("Hello world")
+
+
+# Example Output
+
+# After you have written your code, you should run your program and it should print the following:
+
+# Day 1 - Python Print Function
+
+# The function is declared like this:
+
+# print('what to print')
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
