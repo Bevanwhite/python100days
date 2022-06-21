a86e10dfd4c10489b5d273f625226e2beeacc4ad
Bevanwhite <jefferybevan@gmail.com>
2022-06-21 01:06:23 -0400

    coding challange 2.1 is done

---
+++ day02.py
@@ -0,0 +1,11 @@
+# coding challange 2.1
+# 🚨 Don't change the code below 👇
+# two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+
+# print(int(two_digit_number[0])+ int(two_digit_number[1]))
+
+# BMI calculator
