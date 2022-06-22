39456df8f366d7ce8f285ca756c8929f89a5402c
Bevanwhite <jefferybevan@gmail.com>
2022-06-22 22:49:49 +0530

    fix[issue #8]:BMI 2.0 is completed

--- day03.py
+++ day03.py
@@ -1,9 +1,31 @@
 # 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
+# number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-if( number%2 == 0):
-    print("This is an even number")
+# if( number % 2 == 0):
+#     print("This is an even number")
+# else:
+#     print("This is an Odd number")
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bmi = weight / height ** 2
+
+if(bmi<18.5):
+    weight_description = "you are underweight."
+elif(18.5<bmi<25):
+    weight_description = "you have a normal weight."
+elif(25<bmi<30):
+    weight_description = "you are slightly overweight."
+elif(30<bmi<35):
+    weight_description = "you are obese."
 else:
-    print("This is an Odd number")
+    weight_description = "you are clinically obese."
+    
+print(f"Your BMI is {round(bmi)}, {weight_description}")
