32f7f7c06da662924cf1ebc032b5c04cba328612
Bevanwhite <jefferybevan@gmail.com>
2022-08-03 14:02:50 +0530

    fix[issue#12]:added the correction

--- lovecalculator.py
+++ lovecalculator.py
@@ -9,20 +9,21 @@ lower_name1 = name1.lower()
 lower_name2 = name2.lower()
 lower_name = lower_name1 + lower_name2
 
-a1 = lower_name.count("t")
-b1 = lower_name.count("r")
-c1 = lower_name.count("u")
-d1 = lower_name.count("e")
+count_t = lower_name.count("t")
+count_r = lower_name.count("r")
+count_u = lower_name.count("u")
+count_e = lower_name.count("e")
 
-x = a1+b1+c1+d1 
+count_of_true = count_t + count_r + count_u + count_e
 
-a2 = lower_name.count("l")
-b2 = lower_name.count("o")
-c2 = lower_name.count("v")
-d2 = lower_name.count("e")
+count_l = lower_name.count("l")
+count_o = lower_name.count("o")
+count_v = lower_name.count("v")
+count_e = lower_name.count("e")
 
-y = a2+b2+c2+d2
-lovescore= int(str(x) + str(y))
+count_of_love = count_l + count_o + count_v + count_e
+
+lovescore = int(str(count_of_true) + str(count_of_love))
 
 if (lovescore < 10 or lovescore > 90):
     print(f"Your score is {lovescore}, you go together like coke and mentos")
